a617f124bc9f6b5ad1bc778eb7aa730262ab0b24
GuiBret <guillaume.bretzner@gmail.com>
2021-05-10 00:41:57 +0200

    Add tests

--- database/feeds.go
+++ database/feeds.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
@@ -10,15 +11,20 @@ type FeedBody struct {
 	Name string `json:"name"`
 }
 
+func HandleConnectionError(err error, w http.ResponseWriter) {
+	fmt.Fprintf(w, "DB connection error : %s", err.Error())
+	w.WriteHeader(http.StatusInternalServerError)
+}
+
 func CreateFeed(body FeedBody, w http.ResponseWriter) (uint, error) {
 	db, err := GetConnection()
 
 	if err != nil {
-		fmt.Fprintf(w, "DB connection error : %s", err.Error())
-		w.WriteHeader(http.StatusInternalServerError)
+		HandleConnectionError(err, w)
 		return 0, err
 
 	}
+
 	feed := Feed{
 		Url:  body.Url,
 		Name: body.Name,
@@ -26,4 +32,50 @@ func CreateFeed(body FeedBody, w http.ResponseWriter) (uint, error) {
 
 	db.Create(&feed)
 	return feed.ID, nil
+}
+
+func GetFeed(id uint, w http.ResponseWriter) (Feed, error) {
+	db, err := GetConnection()
+
+	if err != nil {
+		HandleConnectionError(err, w)
+		return Feed{}, err
+
+	}
+
+	feed := Feed{}
+
+	result := db.Find(&feed, "id = ?", id)
+
+	if result.RowsAffected == 0 {
+
+		w.WriteHeader(http.StatusNotFound)
+		return feed, errors.New(fmt.Sprintf("No record found for %d", id))
+	}
+
+	return feed, nil
+}
+
+func DeleteFeed(id uint, w http.ResponseWriter) error {
+
+	db, err := GetConnection()
+
+	if err != nil {
+		HandleConnectionError(err, w)
+		return err
+
+	}
+
+	feed := Feed{}
+
+	result := db.Find(&feed, "id = ?", id)
+
+	if result.RowsAffected == 0 {
+		return errors.New(fmt.Sprintf("The entity with ID %d does not exist", id))
+	}
+
+	db.Delete(&Feed{}, id)
+
+	return nil
+
 }
--- database/feeds_test.go
+++ database/feeds_test.go
@@ -21,3 +21,99 @@ func TestCreateFeed(t *testing.T) {
 		}
 	})
 }
+
+func TestGetFeed(t *testing.T) {
+	t.Run("Should return an error since the entity does not exist", func(t *testing.T) {
+		db, err := GetConnection()
+
+		// First, we need the max ID in the DB
+		if err != nil {
+			panic("Test error")
+		}
+
+		feed := Feed{}
+
+		db.Last(&feed)
+
+		maxId := feed.ID
+
+		mockRR := httptest.NewRecorder()
+
+		_, err = GetFeed(maxId+5, mockRR)
+
+		if err == nil {
+
+			t.Errorf("Expected an error but did not get one")
+		}
+
+	})
+	t.Run("Should return the entity since it exists", func(t *testing.T) {
+
+		// First, we need to create an entity
+		feed := FeedBody{
+			Name: "a",
+			Url:  "https://def.com/feeds",
+		}
+		mockRR := httptest.NewRecorder()
+
+		id, _ := CreateFeed(feed, mockRR)
+
+		_, err := GetFeed(id, mockRR)
+
+		if err != nil {
+
+			t.Errorf("Expected no error but got %s", err.Error())
+		}
+
+	})
+}
+
+func TestDeleteFeed(t *testing.T) {
+	t.Run("Should not have done anything since the element does not exist", func(t *testing.T) {
+
+		db, err := GetConnection()
+		mockRR := httptest.NewRecorder()
+
+		// First, we need the max ID in the DB
+		if err != nil {
+			panic("Test error")
+		}
+
+		feed := Feed{}
+
+		db.Last(&feed)
+
+		maxId := feed.ID
+
+		err = DeleteFeed(maxId+5, mockRR)
+
+		if err == nil {
+			t.Errorf("Expected an error but did not get one")
+		}
+	})
+	t.Run("Should have deleted the entity we have just created", func(t *testing.T) {
+
+		mockRR := httptest.NewRecorder()
+
+		// First, we need to create an entity
+		feed := FeedBody{
+			Name: "a",
+			Url:  "https://def.com/feeds",
+		}
+
+		id, _ := CreateFeed(feed, mockRR)
+
+		err := DeleteFeed(id, mockRR)
+
+		if err != nil {
+			t.Errorf("Expected no error but got %s", err.Error())
+		}
+
+		_, err = GetFeed(id, mockRR)
+
+		if err == nil {
+			t.Errorf("Expected an error but did not get one")
+		}
+
+	})
+}
