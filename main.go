9c77990780fc08fbb97c976f83e3f078ebb84545
GuiBret <guillaume.bretzner@gmail.com>
2021-05-09 22:39:34 +0200

    Add test

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
 	"rss-picker-api/database"
 
@@ -13,16 +16,29 @@ var db *gorm.DB
 
 type Feed struct {
 	gorm.Model
-	Id  int
-	Url string
+	Url  string `json:"url"`
+	Name string `json:"name"`
 }
 
 type Group struct {
 	gorm.Model
-	id           int
-	name         string
+	Name         string `json:"name"`
 	FeedsInGroup []Feed `gorm:"many2many:group_feed;"`
 }
+
+type FeedList struct {
+	Feeds []Feed `json:"feeds"`
+}
+
+type GroupList struct {
+	Groups []Group `json:"groups"`
+}
+
+type GroupBody struct {
+	Name string `json:"name"`
+}
+
+var group Group
 
 func main() {
 
@@ -49,12 +65,67 @@ func handleRequests() {
 	myRouter.HandleFunc("/groups/{feedId}/feeds/{feedId}", AddFeedToGroup).Methods("POST")
 	myRouter.HandleFunc("/groups/{feedId}/feeds/{feedId}", DeleteFeedFromGroup).Methods("DELETE")
 
+	myRouter.HandleFunc("/feeds", ListFeeds).Methods("GET")
+	myRouter.HandleFunc("/feeds", AddFeed).Methods("POST")
+	myRouter.HandleFunc("/feeds", DeleteFeed).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":4005", myRouter))
+
 }
 
 func ListGroups(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Add("Content-Type", "application/json")
+
+	db, err := database.GetConnection()
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	var groups []Group
+	var groupResult GroupList
+
+	db.Find(&groups)
+
+	groupResult.Groups = groups
+
+	json.NewEncoder(w).Encode(groupResult)
+
 }
 
 func AddGroup(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+
+	body := GroupBody{}
+	err := decoder.Decode(&body)
+
+	db, err := database.GetConnection()
+
+	if err != nil {
+		fmt.Printf(err.Error())
+
+		http.Error(w, "Error", http.StatusInternalServerError)
+		return
+	}
+
+	if body.Name == "" {
+		http.Error(w, "Missing value 'name'", http.StatusBadRequest)
+		return
+	}
+
+	group := Group{
+		Name:         body.Name,
+		FeedsInGroup: []Feed{},
+	}
+
+	fmt.Printf("%v", group)
+
+	db.Create(&group)
+
+	w.WriteHeader(http.StatusCreated)
+
+	json.NewEncoder(w).Encode(nil)
 
 }
 
@@ -71,5 +142,32 @@ func AddFeedToGroup(w http.ResponseWriter, r *http.Request) {
 }
 
 func DeleteFeedFromGroup(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func ListFeeds(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Add("Content-Type", "application/json")
+
+	db, err := database.GetConnection()
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	var groups []Feed
+	var feedResult FeedList
+
+	db.Find(&groups)
+
+	feedResult.Feeds = groups
+
+	json.NewEncoder(w).Encode(feedResult)
+}
+
+func AddFeed(w http.ResponseWriter, r *http.Request) {
+
+}
+func DeleteFeed(w http.ResponseWriter, r *http.Request) {
 
 }
--- main_test.go
+++ main_test.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -22,9 +22,81 @@ func TestListGroups(t *testing.T) {
 		body, _ := ioutil.ReadAll(response.Body)
 		err := json.Unmarshal(body, &v)
 
-		fmt.Printf("%s", response.Body.String())
 		if err != nil {
 			t.Errorf("Type error : %s", err)
 		}
+	})
+}
+
+func TestListFeeds(t *testing.T) {
+	t.Run("Should return a list of feeds", func(t *testing.T) {
+		req, _ := http.NewRequest("GET", "http://localhost:4005/feeds", nil)
+
+		v := FeedList{}
+
+		response := httptest.NewRecorder()
+
+		ListFeeds(response, req)
+
+		body, _ := ioutil.ReadAll(response.Body)
+
+		err := json.Unmarshal(body, &v)
+
+		if err != nil {
+			t.Errorf("Type error : %s", err)
+		}
+
+	})
+}
+
+func TestAddGroup(t *testing.T) {
+	t.Run("Should return an error since the body is invalid", func(t *testing.T) {
+		body := strings.NewReader(`{}`)
+
+		req, _ := http.NewRequest("POST", "http://localhost:4005/groups", body)
+
+		rr := httptest.NewRecorder()
+
+		handler := http.HandlerFunc(AddGroup)
+
+		handler.ServeHTTP(rr, req)
+
+		if status := rr.Code; status != http.StatusBadRequest {
+			t.Errorf("Expected HTTP 400 but got %d", status)
+		}
+
+	})
+	t.Run("Should return an error since name is not passed", func(t *testing.T) {
+		body := strings.NewReader(`{}`)
+
+		req, _ := http.NewRequest("POST", "http://localhost:4005/groups", body)
+
+		rr := httptest.NewRecorder()
+
+		handler := http.HandlerFunc(AddGroup)
+
+		handler.ServeHTTP(rr, req)
+
+		if status := rr.Code; status != http.StatusBadRequest {
+			t.Errorf("Expected HTTP 400 but got %d", status)
+		}
+
+	})
+
+	t.Run("Should return HTTP Created since all fields are passed", func(t *testing.T) {
+		body := strings.NewReader(`{"name": "CNN"}`)
+
+		req, _ := http.NewRequest("POST", "http://localhost:4005/groups", body)
+
+		rr := httptest.NewRecorder()
+
+		handler := http.HandlerFunc(AddGroup)
+
+		handler.ServeHTTP(rr, req)
+
+		if status := rr.Code; status != http.StatusCreated {
+			t.Errorf("Expected HTTP 201 but got %d", status)
+		}
+
 	})
 }
